9a5ae982d084bfc111430c1cf9b38b781459a8d7
Harri <harri.fernandez@gmail.com>
2016-05-09 15:29:36 +0000

    Nuevo código para correr el servidor en Cloud9

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-								    return render_template('index.html')
-
+	return render_template('index.html')
+import os
+app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 if __name__ == '__main__':
 								    app.run(
 								        host="0.0.0.0",
