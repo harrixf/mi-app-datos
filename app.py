63490a9712fde416b9b4cacaf6f2d3539ef1e04d
Harri <harri.fernandez@gmail.com>
2016-05-13 11:37:38 +0000

    He modificado el script que daba error. El codigo python era incorrecto

--- app.py
+++ app.py
@@ -1,30 +1,32 @@
+
 import csv
+import os
+from flask import Flask
+from flask import render_template
+from flask import send_from_directory
+app = Flask(__name__, static_url_path='')
+
+
 csv_path = './static/incendios.csv'
 csv_obj = csv.DictReader(open(csv_path, 'r'))
 csv_list = list(csv_obj)
 csv_dict = dict([[o['IDPIF'], o] for o in csv_list])
 
-import os
-from flask import Flask
-from flask import render_template
-from flask import send_from_directory
-app = Flask(__name__)
-
 @app.route("/")
 def index():
-	return render_template('index.html',
-	object_list=csv_list)
+    return render_template('index.html',
+        object_list=csv_list,
+    )
 
-@app.route('/libs/<path:path>') 
-def send_files(path): 
-	return send_from_directory('libs', path) 
-
+@app.route('/libs/<path:path>')
+def send_js(path):
+    return send_from_directory('libs', path)
 
 @app.route('/<number>/')
 def detail(number):
-	return render_template('detail.html',
-	object=csv_dict[number],
-								    )
+    return render_template('detail.html',
+        object=csv_dict[number],
+    )
 
 app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 if __name__ == '__main__':
@@ -34,4 +36,3 @@ if __name__ == '__main__':
 								        use_reloader=True,
 								        debug=True,
 								    )
-'</number>'
